b305e06820de27a96a26ff3c1f23d78c13849939
Hoang Nguyen <hoang.nguyenvan@oivan.com>
2025-06-28 21:45:12 +0700

    Feature: Add sidekiq gem to application

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module RedisResearch
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    # load config here
 
     # Configuration for the application, engines, and railties goes here.
     #
